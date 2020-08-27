8450e473cfb519072619bf6ff28db477aaa72e77
zlatan-ubuntu <zlatangoralija@yahoo.com>
2020-08-27 20:33:54 +0200

    database config

--- btre/settings.py
+++ btre/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btre_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
