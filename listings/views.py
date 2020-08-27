ce1ff4f9c83c1d4cc04568be0e7854f708ae240e
zlatangoralija <zlatangoralija@yahoo.com>
2020-08-27 19:59:18 +0200

    listings and realtors apps, listings static pages

---
+++ listings/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'listings/index.html')
+
+def listing(request, listing_id):
+    return render(request, 'listings/listing.html')
+
+def search(request):
+    return render(request, 'listings/search.html')
