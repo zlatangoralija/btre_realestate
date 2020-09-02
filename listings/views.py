af83aa61dac42bfa395debc7f355ff039f076fa9
zlatan-ubuntu <zlatangoralija@yahoo.com>
2020-09-02 19:25:39 +0200

    prepare search form

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -21,4 +22,10 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices,
+    }
+    return render(request, 'listings/search.html', context)
