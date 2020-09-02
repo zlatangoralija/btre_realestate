ebb506531d28f1d0756fbb6f734f96b5ea8ccc45
zlatan-ubuntu <zlatangoralija@yahoo.com>
2020-09-02 21:01:55 +0200

    listings filtering

--- listings/views.py
+++ listings/views.py
@@ -22,8 +22,31 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    keywords = request.GET['keywords'] if 'keywords' in request.GET else None
+    city = request.GET['city'] if 'city' in request.GET else None
+    state = request.GET['state'] if 'state' in request.GET else None
+    bedrooms = request.GET['bedrooms'] if 'bedrooms' in request.GET else None
+    price = request.GET['price'] if 'price' in request.GET else None
+
+    if keywords:
+        queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    if city:
+        queryset_list = queryset_list.filter(city__iexact=city)
+
+    if state:
+        queryset_list = queryset_list.filter(state__iexact=state)
+
+    if bedrooms:
+        queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    if price:
+        queryset_list = queryset_list.filter(price__lte=price)
 
     context = {
+        'listings': queryset_list,
         'price_choices': price_choices,
         'bedroom_choices': bedroom_choices,
         'state_choices': state_choices,
